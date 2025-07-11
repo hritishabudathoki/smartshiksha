efd019b38988d8f81f039443321f09666e7f8f49
superxd2222 <sumisonxd@gmail.com>
2025-07-11 16:18:53 +0545

    homepage styles added and router updated

--- SMARTSHIKSHA/src/App.js
+++ SMARTSHIKSHA/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
         
       </Routes>
