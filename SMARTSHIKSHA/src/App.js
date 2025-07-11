6f722ce80f0b3cb8ba6d3218d2aa66087cd49b60
superxd2222 <sumisonxd@gmail.com>
2025-07-11 16:09:42 +0545

    update files and replaced error handling

--- SMARTSHIKSHA/src/App.js
+++ SMARTSHIKSHA/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './components/HomePage';
 import Login from './public/Login';
 
 
@@ -9,7 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         
       </Routes>
